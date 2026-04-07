baed565b9629c76c8608289f861e78c97af91c7e
pepemejia <60108780+pepemejia@users.noreply.github.com>
2026-04-07 14:28:22 -0600

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.KFRESITA_SUPABASE = {
   url: 'https://phlrebztjoafdhbsdpxu.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBobHJlYnp0am9hZmRoYnNkcHh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1ODc5NTksImV4cCI6MjA5MTE2Mzk1OX0.gSKOHnkRC_6HKTbQeskK7DC76RiFhbp0uU2On3mH0CI'
+  anonKey: 'sb_publishable_Hz8BHfZgl5UrYtKnQjvS1A_HJ7JlvwS'
 };
